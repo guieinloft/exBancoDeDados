3fa96be3bb5f6094e1b175ff4f20f5c5bf1219ae
guikage <guieinloft@skiff.com>
2024-06-15 13:59:51 -0300

    ajustando topico 20

--- movies4.sql
+++ movies4.sql
@@ -3,7 +3,7 @@
 use movies;
 -- 1. Selecionar filmes que possuam artistas com mais do que 1.9m de altura. 
 -- Exibir título do filme, bem como nome do artista e sua altura.
-select f.titulo
+select f.titulo, a.nome, a.altura
 from filme f
 natural join elenco e
 natural join ator a
@@ -27,15 +27,16 @@ where f.idFilmeAnterior is not null;
 -- 5. Exibir nome de diretores que não dirigiram nada entre 2005 e 2010.
 select d.nome
 from diretor d
-natural left join filme f
-where f.ano between 2005 and 2010 and f.idFilme is null;
+left join filme f on d.idDiretor = f.idDiretor and f.ano between 2005 and 2010
+where f.idFilme is null;
 -- 6.	Exibir nome de artistas e a bilheteria somada de todos seus filmes. 
 -- Ordenar pela bilheteria.
 select a.nome, sum(f.bilheteria)
 from ator a 
 natural inner join elenco e
 natural inner join filme f
-group by a.idAtor;
+group by a.idAtor
+order by sum(f.bilheteria);
 -- 7. Exibir a maior e a menor bilheteria de cada artista.
 -- Apenas para artistas que tenha pelo menos dois filmes.
 select a.nome, max(f.bilheteria), min(f.bilheteria)
@@ -53,20 +54,18 @@ inner join elenco on filme.idFilme = elenco.idFilme
 inner join ator on elenco.idAtor = ator.idAtor
 group by diretor.idDiretor, ator.idAtor;
 -- 9. Exibir título de filmes que tiveram participantes do sexo feminino.
-select filme.titulo
+select distinct filme.titulo
 from filme
 inner join elenco on filme.idFilme = elenco.idFilme
 inner join ator on elenco.idAtor = ator.idAtor
 where ator.sexo = "F";
 -- 10. Exibir título de filmes que não tiveram participantes do sexo feminino.
-select filme.titulo
+select distinct filme.titulo
 from filme
-where filme.idFilme not in
-(select filme.idFilme
-from filme
-inner join elenco on filme.idFilme = elenco.idFilme
-inner join ator on elenco.idAtor = ator.idAtor
-where ator.sexo = "F");
+left join elenco on filme.idFilme = elenco.idFilme
+left join ator on elenco.idAtor = ator.idAtor and ator.sexo = 'F'
+group by filme.idFilme
+having count(ator.idAtor) = 0;
 -- Os próximos exercícios não serão avaliados, mas podem ser entregues junto com os demais.
 
 -- 11. Para cada diretor, exibir seu nome e a quantidade de artistas do sexo feminino com os quais ele já trabalhou. 
@@ -83,8 +82,7 @@ select diretor.nome, count(distinct ator.idAtor)
 from diretor
 left join filme on diretor.idDiretor = filme.idDiretor
 left join elenco on filme.idFilme = elenco.idFilme
-left join ator on elenco.idAtor = ator.idAtor
-where ator.pais like "Inglaterra"
+left join ator on elenco.idAtor = ator.idAtor and ator.pais like "Inglaterra"
 group by diretor.idDiretor;
 -- 13. Exibir título de filmes que tiveram participantes tanto do sexo masculino quanto feminino.
 select filme.titulo
@@ -101,8 +99,22 @@ inner join elenco on filme.idFilme = elenco.idFilme
 inner join ator on elenco.idAtor = ator.idAtor
 where ator.sexo = "M");
 -- 14. Para cada par de artistas que contracenou junto, mostrar o número de filmes em que ambos apareceram.
-select count(filme.idFilme)
-from ator
--- 15. Mostrar títulos do filmes em que tanto Michael Cane quanto Morgan Freeman contracenaram.
-
--- 16. Retorne quantos filmes estão catalogados considerando a primeira letra do título. 
+select a1.nome, a2.nome, count(*)
+from filme
+inner join elenco e1 on filme.idFilme = e1.idFilme
+inner join ator a1 on e1.idAtor = a1.idAtor
+inner join elenco e2 on filme.idFilme = e2.idFilme
+inner join ator a2 on e2.idAtor = a2.idAtor
+where a1.idAtor < a2.idAtor
+group by a1.idAtor, a2.idAtor;
+-- 15. Mostrar títulos do filmes em que tanto Michael Caine quanto Morgan Freeman contracenaram.
+select filme.titulo from filme
+join elenco e1 on filme.idFilme = e1.idFilme
+join ator a1 on e1.idAtor = a1.idAtor
+join elenco e2 on filme.idFilme = e2.idFilme
+join ator a2 on e2.idAtor = a2.idAtor
+where a1.nome like "Michael Caine" and a2.nome like "Morgan Freeman";
+-- 16. Retorne quantos filmes estão catalogados considerando a primeira letra do título.
+select substring(filme.titulo, 1, 1), count(*)
+from filme
+group by substring(filme.titulo, 1, 1);
